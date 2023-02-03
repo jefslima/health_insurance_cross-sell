be0086a65b7e85340aa480093acaef55062fd437
Jeferson Lima <jeferson_slima@hotmail.com>
2023-02-03 16:40:21 -0300

    Update HealthInsurance.py

--- Webapp/healthinsurance/HealthInsurance.py
+++ Webapp/healthinsurance/HealthInsurance.py
@@ -5,13 +5,13 @@ import pandas as pd
 class HealthInsurance( object ):
 
     def __init__( self ):
-        self.home_path ='/Users/jefersonlima/GitHub/pa004_health_insurance_cross_sell/health_insurance_cross-sell/'
-        self.annual_premium_scaler =                   pickle.load( open( self.home_path + 'src/features/annual_premium_scaler.pkl', 'rb'  ) )
-        self.age_scaler =                              pickle.load( open( self.home_path + 'src/features/age_scaler.pkl', 'rb'   ) )
-        self.vintage_scaler =                          pickle.load( open( self.home_path + 'src/features/vintage_scaler.pkl', 'rb'   ) )
-        self.target_encode_gender_scaler =             pickle.load( open( self.home_path + 'src/features/target_encode_gender_scaler.pkl', 'rb'   ) )
-        self.target_encode_region_code_scaler =        pickle.load( open( self.home_path + 'src/features/target_encode_region_code_scaler.pkl', 'rb'   ) )
-        self.fe_policy_sales_channel_scaler =          pickle.load( open( self.home_path + 'src/features/fe_policy_sales_channel_scaler.pkl', 'rb'   ) )
+        #self.home_path ='/Users/jefersonlima/GitHub/pa004_health_insurance_cross_sell/health_insurance_cross-sell/'
+        self.annual_premium_scaler =                   pickle.load( open( 'features/annual_premium_scaler.pkl', 'rb'  ) )
+        self.age_scaler =                              pickle.load( open( 'features/age_scaler.pkl', 'rb'   ) )
+        self.vintage_scaler =                          pickle.load( open( 'features/vintage_scaler.pkl', 'rb'   ) )
+        self.target_encode_gender_scaler =             pickle.load( open( 'features/target_encode_gender_scaler.pkl', 'rb'   ) )
+        self.target_encode_region_code_scaler =        pickle.load( open( 'features/target_encode_region_code_scaler.pkl', 'rb'   ) )
+        self.fe_policy_sales_channel_scaler =          pickle.load( open( 'features/fe_policy_sales_channel_scaler.pkl', 'rb'   ) )
 
 
     def data_cleaning( self, df1 ):
